957adfa612d84835a77c5e16c6f4aa02689273bb
pttkachuk <petrotkachuk6@gmail.com>
2023-07-13 14:40:55 +0200

    add lesson 02

---
+++ src/Api/user.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: 'https://dummyjson.com',
+});
+
+export const requestUsers = async (skip, limit) => {
+    const { data } = await instance.get(`/users?skip=${skip}&limit=${limit}`);
+    return data;
+};
+
