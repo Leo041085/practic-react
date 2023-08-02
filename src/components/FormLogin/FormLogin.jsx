93aa7ff6e9d4cdcd25da1d7456398768d1b10270
SerhiiMonzhelesov <monzhelesov@gmail.com>
2023-08-02 15:13:20 +0300

    mui

--- src/components/FormLogin/FormLogin.jsx
+++ src/components/FormLogin/FormLogin.jsx
@@ -1,4 +1,5 @@
-import { ErrorMessage, Field, Formik } from 'formik';
+import { Button, CardContent, Stack, TextField } from '@mui/material';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 import { Link } from 'react-router-dom';
 import * as Yup from 'yup';
 
@@ -8,22 +9,53 @@ const FormLogin = ({ onFormSubmit }) => {
       initialValues={{ name: '', email: '' }}
       validationSchema={Yup.object({
         email: Yup.string().email('Invalid email address').required('Required'),
-        password: Yup.string()
-          .required('Required')
+        password: Yup.string().required('Required'),
       })}
       onSubmit={values => onFormSubmit(values)}
     >
       {formik => (
-        <form onSubmit={formik.handleSubmit}>
-          <label htmlFor="email">Email:</label>
-          <Field name="email" type="email" />
-          <ErrorMessage name="email" component="div" />
-          <label htmlFor="password">Password:</label>
-          <Field name="password" type="password" />
-          <ErrorMessage name="password" component="div" />
-          <button type="submit">Log in</button>
-          <Link to="/registration">Registration</Link>
-        </form>
+        <Stack sx={{ margin: '0 auto', width: '400px' }}>
+          <Form>
+            <Stack spacing={6}>
+              <TextField
+                label="Email:"
+                name="email"
+                type="email"
+                onChange={formik.handleChange}
+                error={formik.errors.email}
+                helperText={
+                  formik.errors.email && 'Please enter a valid email address'
+                }
+              />
+              <TextField
+                label="Password:"
+                name="password"
+                type="password"
+                onChange={formik.handleChange}
+                error={formik.errors.password}
+                helperText={
+                  formik.errors.password && 'Please enter your password'
+                }
+              />
+            </Stack>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+              mt={4}
+            >
+              <Button
+                variant="contained"
+                type="submit"
+                size="large"
+                disabled={!formik.isValid}
+              >
+                Log in
+              </Button>
+              <Link to="/registration">Registration</Link>
+            </Stack>
+          </Form>
+        </Stack>
       )}
     </Formik>
   );
